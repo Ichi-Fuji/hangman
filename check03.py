4d8b8280ed4d6e67dd312c3252163b1549ad642d
Ichi-Fuji <ichifuji36@gmail.com>
2020-03-30 22:00:32 +0900

    my first commit

---
+++ check03.py
@@ -0,0 +1,13 @@
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+
+
+class AlwaysPositive:
+    def __init__(self, number):
+        self.n = number
+
+
+
+x = AlwaysPositive(-20)
+y = AlwaysPositive(10)
+
+print(x + y)
